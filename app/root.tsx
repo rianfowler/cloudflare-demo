a20a159dc9051ba5664363bf5929134707097b7f
Rian Fowler <rianf@me.com>
2024-04-06 15:37:19 -0500

    remove changes start over

--- app/root.tsx
+++ app/root.tsx
@@ -1,42 +1,12 @@
-import { json, redirect } from "@remix-run/node";
 import {
-  Form,
-  Link,
   Links,
   Meta,
-  NavLink,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useNavigation,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/server-runtime";
 
-import applyStylesHref from './app.css?url'
-
-import { createEmptyContact, getContacts } from "./data";
-
-export const action = async () => {
-  const contact = await createEmptyContact();
-  return redirect(`/contacts/${contact.id}/edit`)
-};
-
-// Every route can export links function
-// they get collected and rendered to the root <Links />
-export const links: LinksFunction = () => [
-  { rel: "stylesheet", href: applyStylesHref }
-]
-
-export const loader = async () => {
-  const contacts = await getContacts();
-  return json({ contacts });
-};
-
-export default function App() {
-  const { contacts } = useLoaderData<typeof loader>();
-  const navigation = useNavigation();
-
+export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
@@ -46,73 +16,14 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <div id="sidebar">
-          <h1>Remix Contacts</h1>
-          <div>
-            <Form id="search-form" role="search">
-              <input
-                id="q"
-                aria-label="Search contacts"
-                placeholder="Search"
-                type="search"
-                name="q"
-              />
-              <div id="search-spinner" aria-hidden hidden={true} />
-            </Form>
-            <Form method="post">
-              <button type="submit">New</button>
-            </Form>
-          </div>
-          <nav>
-            {contacts.length ? (
-              <ul>
-                {contacts.map((contact) => (
-                  <li key={contact.id}>
-                    <NavLink
-                      className={({ isActive, isPending }) =>
-                        isActive
-                          ? "active"
-                          : isPending
-                            ? "pending"
-                            : ""
-                      }
-                      to={`contacts/${contact.id}`}
-                    >
-                      {/* existing elements */}
-                      <Link to={`contacts/${contact.id}`}>
-                        {contact.first || contact.last ? (
-                          <>
-                            {contact.first} {contact.last}
-                          </>
-                        ) : (
-                          <i>No Name</i>
-                        )}{" "}
-                        {contact.favorite ? (
-                          <span>★</span>
-                        ) : null}
-                      </Link>
-                    </NavLink>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>
-                <i>No contacts</i>
-              </p>
-            )}
-          </nav>
-        </div>
-        <div
-          className={
-            navigation.state === "loading" ? "loading" : ""
-          }
-          id="detail"
-        >
-          <Outlet />
-        </div>
+        {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}
+
+export default function App() {
+  return <Outlet />;
 }
--- load-context.ts
+++ load-context.ts
@@ -1,4 +1,3 @@
-import { type AppLoadContext } from "@remix-run/cloudflare";
 import { type PlatformProxy } from "wrangler";
 
 // When using `wrangler.toml` to configure bindings,
@@ -16,18 +15,3 @@ declare module "@remix-run/cloudflare" {
     cloudflare: Cloudflare;
   }
 }
-
-type GetLoadContext = (args: {
-  request: Request;
-  context: { cloudflare: Cloudflare }; // load context _before_ augmentation
-}) => AppLoadContext;
-
-// Shared implementation compatible with Vite, Wrangler, and Cloudflare Pages
-export const getLoadContext: GetLoadContext = ({
-  context,
-}) => {
-  return {
-    ...context,
-    extra: "stuff",
-  };
-};
